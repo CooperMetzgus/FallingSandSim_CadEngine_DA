6f688578b30d9cf758c0fe8deec39839f2447a7a
Digital Arcana <coopermetzgus@yahoo.com>
2025-01-28 04:22:47 -0800

    Scroll to scale brush size A & D to swap particle types

--- particleMan.cpp
+++ particleMan.cpp
@@ -1,16 +1,57 @@
 #include "particleMan.h"
 
-std::vector<std::shared_ptr <Engine::engineObject>> ParticleMan::particleTypes;
 std::vector<std::vector<bool>> ParticleMan::particleGrid;
-std::shared_ptr <Engine::engineObject> controller;
+std::vector<ParticleMan::particleObject> ParticleMan::particleTypes;
+std::vector<std::shared_ptr <Engine::engineObject>> ParticleMan::activeParticles;
+std::shared_ptr <ParticleMan::brushObject> brush;
 
-void gravity(std::shared_ptr<Engine::engineObject> obj) {
+
+void emptyParticle(std::shared_ptr<Engine::engineObject> obj) {
+	auto particleObj = std::dynamic_pointer_cast<ParticleMan::particleObject>(obj);
+}
+
+void dirtParticle(std::shared_ptr<Engine::engineObject> obj) {
+	auto particleObj = std::dynamic_pointer_cast<ParticleMan::particleObject>(obj);
+
+	std::vector<SDL_FPoint> checkDirections; //Create Empty List of Directions
+	for (int i = 0; i < checkDirections.size(); i++) { //For Each Item in Direction List
+		if (particleObj->tryMove(checkDirections[i]))
+			return;
+	}
+}
+
+void sandParticle(std::shared_ptr<Engine::engineObject> obj) {
+	auto particleObj = std::dynamic_pointer_cast<ParticleMan::particleObject>(obj);
+
+	std::vector<SDL_FPoint> checkDirections; //Create Empty List of Directions
+		checkDirections.push_back({ 0, 1 }); //Add Down to Directions List
+	if (rand() % 2) {
+		checkDirections.push_back({ 1, 1 }); //Add Down Left
+		checkDirections.push_back({ -1, 1 }); //Add Down Right
+	}
+	for (int i = 0; i < checkDirections.size(); i++) { //For Each Item in Direction List
+		if (particleObj->tryMove(checkDirections[i])) // If Direction Checked is Usable
+			return; //Actually Move Particle
+	}
+}
+
+void waterParticle(std::shared_ptr<Engine::engineObject> obj) {
 	auto particleObj = std::dynamic_pointer_cast<ParticleMan::particleObject>(obj);
 
 	std::vector<SDL_FPoint> checkDirections; //Create Empty List of Directions
 	checkDirections.push_back({ 0, 1 }); //Add Down to Directions List
-	checkDirections.push_back({ 1, 1 }); //Add Down Left
-	checkDirections.push_back({ -1, 1 }); //Add Down Right
+	if (rand() % 2) {
+		checkDirections.push_back({ 1, 1 }); //Add Down Left
+		checkDirections.push_back({ -1, 1 }); //Add Down Right
+	}
+	if (rand() % 2) {
+		checkDirections.push_back({ -1, 0 }); //Add Left
+		checkDirections.push_back({ 1, 0 }); //Add Right
+	}
+	else {
+		checkDirections.push_back({ 1, 0 }); //Add Right
+		checkDirections.push_back({ -1, 0 }); //Add Left
+	}
 	for (int i = 0; i < checkDirections.size(); i++) { //For Each Item in Direction List
 		if (particleObj->tryMove(checkDirections[i]))
 			return;
@@ -28,19 +69,14 @@ void keepInBounds(std::shared_ptr<Engine::engineObject> obj) {
 		obj->hull.y = Engine::baseRes.y-1;
 }
 
-void addParticle(std::shared_ptr<Engine::engineObject> obj) {
-	if (Engine::mouseStates[0] == 2) {
-		if (!ParticleMan::particleGrid[Engine::mousePos.x][Engine::mousePos.y]) { //Add Particle if Space Available
-			std::shared_ptr<ParticleMan::particleObject> velObj = std::make_shared<ParticleMan::particleObject>();
-			ParticleMan::particleTypes.push_back(Engine::addObject(velObj));
-			ParticleMan::particleTypes.back()->hull.x = int(Engine::mousePos.x);
-			ParticleMan::particleTypes.back()->hull.y = int(Engine::mousePos.y);
-			ParticleMan::particleTypes.back()->updateFuncs.push_back(gravity);
-			ParticleMan::particleTypes.back()->updateFuncs.push_back(keepInBounds);
-			ParticleMan::particleGrid[Engine::mousePos.x][Engine::mousePos.y] = true;
-			printf("Particle Total Count: %i\n", ParticleMan::particleTypes.size());
-		}
-	}
+void brushUpdate(std::shared_ptr<Engine::engineObject> obj) {
+	auto brushObj = std::dynamic_pointer_cast<ParticleMan::brushObject>(obj);
+	brushObj->addParticle();
+	brushObj->particleSelection();
+	brushObj->generateBrushScale();
+	brush->tex = ParticleMan::particleTypes[brush->selectedParticle].tex;
+	obj->hull.x = Engine::mousePos.x;
+	obj->hull.y = Engine::mousePos.y;
 }
 
 void ParticleMan::initParticle() {
@@ -53,9 +89,28 @@ void ParticleMan::initParticle() {
 		std::fill(ParticleMan::particleGrid[i].begin(), ParticleMan::particleGrid[i].end(), false);
 	}
 
-	std::shared_ptr<Engine::engineObject> Obj = std::make_shared<Engine::engineObject>();
-	controller = Engine::addObject(Obj);
-	controller->updateFuncs.push_back(addParticle);
+	//Adds Particle to Local List
+	SDL_Color dirt = { 89, 58, 14, 255 }; //Create Particle Color
+	ParticleMan::particleObject dirtPhysics = ParticleMan::particleObject(dirt); //Create Particle as Object
+	dirtPhysics.updateFuncs.push_back(dirtParticle); //Define Particle Physics
+	dirtPhysics.updateFuncs.push_back(keepInBounds); //Keeps Particle in Window Bounds
+	ParticleMan::particleTypes.push_back(dirtPhysics);
+
+	SDL_Color sand = { 235, 235, 180, 255 };
+	ParticleMan::particleObject sandPhysics = ParticleMan::particleObject(sand);
+	sandPhysics.updateFuncs.push_back(sandParticle);
+	sandPhysics.updateFuncs.push_back(keepInBounds);
+	ParticleMan::particleTypes.push_back(sandPhysics);
+
+	SDL_Color water = { 0, 255, 240, 217 };
+	ParticleMan::particleObject waterPhysics = ParticleMan::particleObject(water);
+	waterPhysics.updateFuncs.push_back(waterParticle);
+	waterPhysics.updateFuncs.push_back(keepInBounds);
+	ParticleMan::particleTypes.push_back(waterPhysics);
+
+	brush = std::make_shared<ParticleMan::brushObject>();
+	Engine::addObject(brush);
+	brush->updateFuncs.push_back(brushUpdate);
 }
 
 /*
--- particleMan.h
+++ particleMan.h
@@ -5,8 +5,7 @@ static class ParticleMan {
 public:
 	static std::vector< std::vector<bool>> particleGrid;
 
-	struct particleObject : public Engine::engineObject
-	{
+	struct particleObject : public Engine::engineObject	{
 		SDL_FPoint vel;
 
 		bool unoccupiedSpace(SDL_FPoint changePos) {
@@ -22,10 +21,8 @@ public:
 				return !particleGrid[checkPos.x][checkPos.y];
 		}
 
-		bool tryMove(SDL_FPoint changePos)
-		{
-			if (unoccupiedSpace(changePos))
-			{
+		bool tryMove(SDL_FPoint changePos)	{
+			if (unoccupiedSpace(changePos))	{
 				SDL_FPoint newPos = { hull.x + changePos.x, hull.y + changePos.y };
 
 				//Adjust Particle Position
@@ -39,7 +36,7 @@ public:
 			return false;
 		}
 
-		particleObject(const SDL_FRect& hull = { 0, 0, 1, 1 }, double rot = 0,
+		particleObject(SDL_Color color = { 255, 255, 255, 255 }, const SDL_FRect & hull = { 0, 0, 1, 1 }, double rot = 0,
 			bool centered = false, SDL_FlipMode flip = SDL_FLIP_NONE, float scale = 1.0,
 			SDL_FPoint vel = { 0, 0 }, double spin = 0, int depth = 0)
 			: engineObject(hull, NULL, rot,
@@ -48,12 +45,99 @@ public:
 			vel(vel) {
 			tex = Engine::loadTargetTex({ 1,1 });
 			SDL_Texture* texture = Engine::setRenderTarget(tex);
-			SDL_Color sand = { 235, 235, 180, 255 };
-			Engine::drawRect(hull, sand);
+			Engine::drawRect(hull, color);
 			Engine::setRenderTarget(texture);
 		}
 	};
-	static void initParticle();
 
-	static std::vector<std::shared_ptr <Engine::engineObject>> particleTypes;
+	struct brushObject : Engine::engineObject {
+		int brushSize = 3;
+		int selectedParticle = 0;
+		
+		void addParticle() { //Function adds Active Engine Object
+			if (Engine::mouseStates[0] == 2) {
+				std::vector<SDL_FPoint> scaledBrush;
+				scaledBrush = generateBrushScale();
+				for (int i = 0; i < scaledBrush.size(); i++)
+				if (!particleGrid[scaledBrush[i].x][scaledBrush[i].y]) { //If Nothing in Grid Tile at Mouse Position
+					particleObject passParticleData = ParticleMan::particleTypes[selectedParticle];
+					activeParticles.push_back(Engine::addObject(std::shared_ptr<ParticleMan::particleObject>(new ParticleMan::particleObject(passParticleData))));
+					activeParticles.back()->hull.x = int(scaledBrush[i].x);
+					activeParticles.back()->hull.y = int(scaledBrush[i].y);
+					particleGrid[scaledBrush[i].x][scaledBrush[i].y] = true;
+				}
+			}
+			if (Engine::mouseStates[1] == 2) {
+				if (!particleGrid[Engine::mousePos.x][Engine::mousePos.y]) { //If Nothing in Particle Grid at Mouse Position
+
+				}
+			}
+			if (Engine::mouseStates[2] == 2) {
+				if (particleGrid[Engine::mousePos.x][Engine::mousePos.y]) { //If Something in Particle Grid at Mouse Position
+					particleGrid[Engine::mousePos.x][Engine::mousePos.y] = false;
+				}
+			}
+			//Brush Scaling Controls
+			if (Engine::wheelStates[0] == 1) {
+				brushSize++;
+				printf("Brush Size: %i\n", brushSize);
+			}
+
+			if (Engine::wheelStates[1] == 1) {
+				brushSize--;
+				if (brushSize <= 0) {
+					brushSize = 0;
+					printf("Brush Size: %i\n", brushSize);
+				}
+			}
+		}
+
+		void particleSelection() {
+			if (Engine::keyStates[SDL_SCANCODE_D] == 1) {
+				selectedParticle++;
+				if (selectedParticle >= particleTypes.size())
+					selectedParticle = 0;
+
+				printf("Selected Particle Type: %i\n", selectedParticle);
+			}
+
+			if (Engine::keyStates[SDL_SCANCODE_A] == 1) {
+				selectedParticle--;
+				if (selectedParticle < 0)
+					selectedParticle = particleTypes.size() - 1;
+
+				printf("Selected Particle Type: %i\n", selectedParticle);
+			}
+		}
+
+		std::vector<SDL_FPoint> generateBrushScale() { //List of Vector Points for Brush
+			int particleOffset = brushSize; //Used for Brush Line Start Position
+			std::vector<SDL_FPoint> returnParticles; //Brush Line Values List for Drawing Line
+			for (int i = 0; i < brushSize * 2 + 1; i++) {
+				SDL_FPoint brushLineStart = { (Engine::mousePos.x - particleOffset), (Engine::mousePos.y - i) }; //Brush Line Start Set to Farthest Left Position, Based on Brush Size
+				for (int j = 0; j < ((brushSize - i) * 2) + 1; j++) {
+					returnParticles.push_back({ brushLineStart.x + j, brushLineStart.y }); //Call X to Brush Line Start, Loop to End of Horizontal Distance
+					returnParticles.push_back({ Engine::mousePos.x + j - particleOffset, Engine::mousePos.y + i}); //Call X to Brush Line Start, Call Y to Next Row Down.
+				}
+				particleOffset--; //Move X Left
+			}
+			return returnParticles;
+		}
+		
+		brushObject(SDL_Color color = { 255, 255, 255, 255 }, const SDL_FRect& hull = { 0, 0, 1, 1 }, double rot = 0,
+			bool centered = false, SDL_FlipMode flip = SDL_FLIP_NONE, float scale = 1.0,
+			SDL_FPoint vel = { 0, 0 }, double spin = 0, int depth = 0)
+			: engineObject(hull, NULL, rot,
+				centered, flip, scale,
+				depth) {
+			tex = Engine::loadTargetTex({ 1,1 });
+			SDL_Texture* texture = Engine::setRenderTarget(tex);
+			Engine::drawRect(hull, color);
+			Engine::setRenderTarget(texture);
+		}
+	};
+	static std::vector<particleObject> particleTypes;
+	static void initParticle();
+	static std::vector<std::shared_ptr <Engine::engineObject>> activeParticles;
+
 };
